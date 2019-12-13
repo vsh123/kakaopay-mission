4d644d5227708b79956d39d4ed28995c11f05bcb
SunHo Van <vsh123@naver.com>
2019-12-13 17:56:45 +0900

    feat: RegionSupportInformation

---
+++ src/main/java/kakaopay/domain/RegionSupportInformation.java
@@ -0,0 +1,26 @@
+package kakaopay.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Builder
+@ToString
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class RegionSupportInformation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Region region;
+    private String target;
+    private String usage;
+    private String limit;
+    private String rate;
+    private String institute;
+    private String mgmt;
+    private String reception;
+}
