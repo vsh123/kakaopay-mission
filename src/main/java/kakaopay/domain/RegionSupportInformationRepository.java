bee8ad4ed2031201bb1202d973ad31d5dd6d648d
SunHo Van <vsh123@naver.com>
2019-12-14 19:32:45 +0900

    feat: RegionSupportInformationRepository.findAll(Pageable)

--- src/main/java/kakaopay/domain/RegionSupportInformationRepository.java
+++ src/main/java/kakaopay/domain/RegionSupportInformationRepository.java
@@ -1,9 +1,13 @@
 package kakaopay.domain;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RegionSupportInformationRepository extends JpaRepository<RegionSupportInformation, Long> {
     Optional<RegionSupportInformation> findByRegionCode(String regionCode);
+
+    Page<RegionSupportInformation> findAll(Pageable pageable);
 }
--- src/test/java/kakaopay/domain/RegionSupportInformationRepositoryTest.java
+++ src/test/java/kakaopay/domain/RegionSupportInformationRepositoryTest.java
@@ -3,6 +3,10 @@ package kakaopay.domain;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,5 +33,13 @@ class RegionSupportInformationRepositoryTest {
                         .orElseThrow(IllegalArgumentException::new);
 
         assertThat(expectedRegionSupInfo.getRegion()).isEqualTo(savedRegion);
+    }
+
+    @Test
+    void findOderByTest() {
+        Pageable pageable = PageRequest.of(0, 1);
+        List<RegionSupportInformation> result = regionSupportInformationRepository.findAll(pageable).getContent();
+
+        assertThat(result.size()).isEqualTo(1);
     }
 }
