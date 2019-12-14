01df813ac219e1af6f8d7e970b3ff05dbd5ea88e
SunHo Van <vsh123@naver.com>
2019-12-14 20:29:01 +0900

    feat: Rate minRate, maxRate 추가

--- src/main/java/kakaopay/domain/Rate.java
+++ src/main/java/kakaopay/domain/Rate.java
@@ -9,14 +9,20 @@ import javax.persistence.Embeddable;
 public class Rate {
     @Column(name = "rate_info")
     private String rateInfo;
+    @Column(name = "min_rate")
+    private double minRate;
+    @Column(name = "max_rate")
+    private double maxRate;
     @Column(name = "avg_rate")
     private double averageRate;
 
     private Rate() {
     }
 
-    public Rate(String rateInfo, double averageRate) {
+    public Rate(String rateInfo, double minRate, double maxRate, double averageRate) {
         this.rateInfo = rateInfo;
+        this.minRate = minRate;
+        this.maxRate = maxRate;
         this.averageRate = averageRate;
     }
 
@@ -28,6 +34,14 @@ public class Rate {
         return rateInfo;
     }
 
+    public double getMinRate() {
+        return minRate;
+    }
+
+    public double getMaxRate() {
+        return maxRate;
+    }
+
     public double getAverageRate() {
         return averageRate;
     }
@@ -36,6 +50,8 @@ public class Rate {
     public String toString() {
         return "Rate{" +
                 "rateInfo='" + rateInfo + '\'' +
+                ", minRate=" + minRate +
+                ", maxRate=" + maxRate +
                 ", averageRate=" + averageRate +
                 '}';
     }
